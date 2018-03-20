221bc0956ed1fc8158508ca543ebfa331451bc84
Kevin Garcia <kevinfernandogarcia@gmail.com>
2018-03-20 10:21:57 -0500

    PyQtImageViewer files added

---
+++ Research/Test.py
@@ -0,0 +1,68 @@
+""" QtImageViewer.py: PyQt image viewer widget for a QPixmap in a QGraphicsView scene with mouse zooming and panning.
+Source: https://github.com/marcel-goldschen-ohm/PyQtImageViewer
+"""
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from QtImageViewer import QtImageViewer
+
+
+# Custom slot for handling mouse clicks in our viewer.
+# Just prints the (row, column) matrix index of the 
+# image pixel that was clicked on.
+def handleLeftClick(x, y):
+    row = int(y)
+    column = int(x)
+    print("Pixel (row="+str(row)+", column="+str(column)+")")
+    
+
+if __name__ == '__main__':
+    # Create the QApplication.
+    app = QApplication(sys.argv)
+        
+    # Create an image viewer widget.
+    viewer = QtImageViewer()
+        
+    # Set viewer's aspect ratio mode.
+    # !!! ONLY applies to full image view.
+    # !!! Aspect ratio always ignored when zoomed.
+    #   Qt.IgnoreAspectRatio: Fit to viewport.
+    #   Qt.KeepAspectRatio: Fit in viewport using aspect ratio.
+    #   Qt.KeepAspectRatioByExpanding: Fill viewport using aspect ratio.
+    viewer.aspectRatioMode = Qt.KeepAspectRatio
+    
+    # Set the viewer's scroll bar behaviour.
+    #   Qt.ScrollBarAlwaysOff: Never show scroll bar.
+    #   Qt.ScrollBarAlwaysOn: Always show scroll bar.
+    #   Qt.ScrollBarAsNeeded: Show scroll bar only when zoomed.
+    viewer.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+    viewer.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+    
+    # Allow zooming with right mouse button.
+    # Drag for zoom box, doubleclick to view full image.
+    viewer.canZoom = True
+    
+    # Allow panning with left mouse button.
+    viewer.canPan = True
+        
+    # Load an image to be displayed.
+    if QT_VERSION_STR[0] == '4':
+        fileName = QFileDialog.getOpenFileName(None, "Open image file...")
+    elif QT_VERSION_STR[0] == '5':
+        fileName, dummy = QFileDialog.getOpenFileName(None, "Open image file...")
+    image = QImage(fileName)
+    
+    # Display the image in the viewer.
+    viewer.setImage(image)
+    
+    # Handle left mouse clicks with your own custom slot
+    # handleLeftClick(x, y). (x, y) are image coordinates.
+    # For (row, col) matrix indexing, row=y and col=x.
+    # ImageViewerQt also provides similar signals for
+    # left/right mouse button press, release and doubleclick.
+    viewer.leftMouseButtonPressed.connect(handleLeftClick)
+        
+    # Show the viewer and run the application.
+    viewer.show()
+    sys.exit(app.exec_())
