b6d592a3b1b5e8b2e3756f1cc5a264698f7e5e77
Kevin Garcia <kevinfernandogarcia@gmail.com>
2018-03-25 19:41:54 -0500

    waypoints now scale when the image is scaled and minor improvements to code structure

--- src/overlay/QtImageViewer.py
+++ src/overlay/QtImageViewer.py
@@ -1,34 +1,50 @@
 import os.path
 import collections
-import bisect
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 class QtImageViewer(QGraphicsView):
     
+    # 'add_delete_waypoint_signal' is used to communicate with the class 'OverlayWidget'
     add_delete_waypoint_signal = pyqtSignal(int, str)
 
     def __init__(self):
 
-        # 'cur_path' is used to generate the paths for the .pngs
+        ###################################
+        # Initialize variables
+        ###################################
+
+        # 'cur_path' is used to generate the paths for the images used within the image viewer
         self.cur_path = os.path.dirname(__file__)
 
-        # important for upholding zoom max and min
-        self.zoom_level = 0
+        # the following variables are used to scale the image
         self.zoom_max = 15
-        self.zoom_min = -3
+        self.zoom_min = -2
+        self.zoom_level = 0
 
-        # 'key_array' holds the alphabetical ('A'-'Z') keys associated with the waypoints
+        # the following variables are used to scale waypoints with relation to the image
+        self.wpt_cur_scale = 3.6
+        self.wpt_def_scale = 3.6
+        self.wpt_max_scale = 3.6
+        self.wpt_min_scale = 0.2
+        self.wpt_scale_factor = 0.2
+
+        # 'key_array' holds the alphabetical keys associated with each waypoint
+        # 'key_array' is in the order Z through A
         self.key_array = [] 
         for x in range(0, 26):
             self.key_array.append(chr(ord('Z') - x))
 
-        # 'waypoints' holds all waypoints in LIFO order
+        # 'waypoints' holds all waypoints in the order that they are added to the screen
         self.waypoints = collections.OrderedDict()
 
         # 'waypoint_icon' holds the pin .png as a pixmap
         self.waypoint_icon = QPixmap(os.path.join(self.cur_path, '../../assets/pin.png'))
+
+        ###################################
+        # Create the image viewer
+        ###################################
 
         QGraphicsView.__init__(self)
         self.setRenderHints(QPainter.Antialiasing|QPainter.SmoothPixmapTransform)
@@ -38,6 +54,13 @@ class QtImageViewer(QGraphicsView):
         self.vlayout.setAlignment(Qt.AlignTop|Qt.AlignRight)
         self.vlayout.addSpacing(80)
 
+        # 'hlayout' controls the horizontal placement of the onscreen buttons
+        self.hlayout = QHBoxLayout()
+        self.hlayout.setAlignment(Qt.AlignRight)
+        self.hlayout.addSpacing(110)
+        self.vlayout.addLayout(self.hlayout)
+
+        # directly below, five buttons are added to the layout of the image viewer
         zoom_in_btn = QPushButton()
         zoom_in_btn.setFixedSize(QSize(45, 45))
         zoom_in_btn.setIcon(QIcon(os.path.join(self.cur_path, '../../assets/zoom_in.png')))
@@ -78,15 +101,10 @@ class QtImageViewer(QGraphicsView):
         undo_wpt_btn.clicked.connect(self.undo_wpt_btn_press)
         self.vlayout.addWidget(undo_wpt_btn)
 
+        # the onscreen button layout is set
         self.setLayout(self.vlayout)
 
-        # 'hlayout' controls the horizontal placement of the onscreen buttons
-        self.hlayout = QHBoxLayout()
-        self.hlayout.setAlignment(Qt.AlignRight)
-        self.hlayout.addSpacing(110)
-        self.vlayout.addLayout(self.hlayout)
-
-    # 'set_image' is called by 'OverlayWidget' to set the .tif image
+    # 'set_image' is called by 'OverlayWidget' to add the .tif image to the image viewer
     def set_image(self, str):
         self.image_path = str
         self.pixmap = QPixmap(self.image_path)
@@ -95,21 +113,69 @@ class QtImageViewer(QGraphicsView):
         self.setScene(self.scene)
         self.fitInView(self.scene.sceneRect(), Qt.KeepAspectRatio)
 
+    # 'zoom_in' handles the zooming of the image and the related scaling of the waypoints
+    def zoom_in(self):
+        if self.zoom_level < self.zoom_max:
+            self.scale(1.25, 1.25)
+            self.zoom_level += 1
+            if self.wpt_cur_scale > self.wpt_min_scale:
+                self.wpt_cur_scale -= self.wpt_scale_factor
+            if self.waypoints:
+                for key, val in self.waypoints.items():
+                    val.setScale(self.wpt_cur_scale)
+
+    # 'zoom_out' handles the zooming of the image and the related scaling of the waypoints
+    def zoom_out(self):
+        if self.zoom_level > self.zoom_min:
+            self.scale(0.8, 0.8)
+            self.zoom_level -= 1
+            if self.wpt_cur_scale < self.wpt_max_scale:
+                self.wpt_cur_scale += self.wpt_scale_factor
+            if self.waypoints:
+                for key, val in self.waypoints.items():
+                    val.setScale(self.wpt_cur_scale)
+
+    # 'delete_waypoint' deletes the specified waypoint if it exists
+    def delete_waypoint(self, _key):
+        if self.waypoints and _key not in self.key_array:
+            self.scene.removeItem(self.waypoints[_key])
+            self.key_array.append(_key)
+            self.key_array.sort(reverse = True)
+            del self.waypoints[_key]
+
+    # 'add_waypoint' adds a waypoint to the image if there are less than 26 on screen
+    def add_waypoint(self, x, y):
+        if self.key_array:
+            _key = self.key_array.pop()
+            _alpha_pin_path = '../../assets/pins/pin_' + _key + '.png'
+            self.waypoint_icon = QPixmap(os.path.join(self.cur_path, _alpha_pin_path))
+            self.waypoint = QGraphicsPixmapItem(self.waypoint_icon)
+            self.waypoint.setTransformOriginPoint(0, self.waypoint_icon.height())
+            self.waypoint.setPos(x, y - self.waypoint_icon.height())
+            self.waypoint.setScale(self.wpt_cur_scale)
+            self.scene.addItem(self.waypoint)
+            self.waypoints[_key] = self.waypoint
+            self.add_delete_waypoint_signal.emit(1, _key)
+
+    ###################################
+    # Button press functions
+    ###################################
+
     def zoom_in_btn_press(self):
         self.setTransformationAnchor(QGraphicsView.AnchorViewCenter)
-        if self.zoom_level < self.zoom_max:
-        	self.scale(1.25, 1.25)
-        	self.zoom_level += 1
+        self.zoom_in()
 
     def zoom_out_btn_press(self):
         self.setTransformationAnchor(QGraphicsView.AnchorViewCenter)
-        if self.zoom_level > self.zoom_min:
-        	self.scale(0.8, 0.8)
-        	self.zoom_level -= 1
+        self.zoom_out()
 
     def expand_btn_press(self):
         self.fitInView(self.scene.sceneRect(), Qt.KeepAspectRatio)
         self.zoom_level = 0
+        if self.waypoints:
+            self.wpt_cur_scale = self.wpt_def_scale
+            for key, val in self.waypoints.items():
+                val.setScale(self.wpt_def_scale)
 
     def visibility_wpts_btn_press(self):
         if self.waypoints and self.waypoints[list(self.waypoints.keys())[0]].isVisible():
@@ -123,48 +189,29 @@ class QtImageViewer(QGraphicsView):
         if self.waypoints:
             self.add_delete_waypoint_signal.emit(0, list(self.waypoints.keys())[-1])
 
-    # 'delete_waypoint' deletes the specified waypoint if it exists
-    def delete_waypoint(self, _key):
-        if self.waypoints and _key not in self.key_array:
-            self.scene.removeItem(self.waypoints[_key])
-            self.key_array.append(_key)
-            self.key_array.sort(reverse=True)
-            del self.waypoints[_key]
+    ###################################
+    # Mouse interaction functions
+    ###################################
 
-    # 'add_waypoint' adds a waypoint to the image if there are remaining keys in the 'key_array'
-    def add_waypoint(self, x, y):
-        if self.key_array:
-            _key = self.key_array.pop()
-            _alpha_pin_path = '../../assets/pins/pin_' + _key + '.png'
-            self.waypoint_icon = QPixmap(os.path.join(self.cur_path, _alpha_pin_path))
-            self.waypoint = QGraphicsPixmapItem(self.waypoint_icon)
-            self.waypoint.setPos(x, y)
-            self.scene.addItem(self.waypoint)
-            self.waypoints[_key] = self.waypoint
-            self.add_delete_waypoint_signal.emit(1, _key)
-
-    # 'wheelEvent' is used for scroll zooming the image
+    # 'wheelEvent' is used to register zooming of the image with the mouse scroll wheel
     def wheelEvent(self, event):
         self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
-        if event.angleDelta().y() > 0 and self.zoom_level < self.zoom_max:
-            factor = 1.25
-            self.zoom_level += 1
-            self.scale(factor, factor)
-        elif event.angleDelta().y() < 0 and self.zoom_level > self.zoom_min:
-            factor = 0.8
-            self.zoom_level -= 1
-            self.scale(factor, factor)
+        if event.angleDelta().y() > 0:
+            self.zoom_in()
+        else:
+            self.zoom_out()
 
-    # 'mousePressEvent' is used for panning of the image and adding waypoints
+    # 'mousePressEvent' is to register panning of the image with the left mouse button
+    # 'mousePressEvent' is to register the adding of waypoints with the right mouse button
     def mousePressEvent(self, event):
         scenePos = self.mapToScene(event.pos())
         if event.button() == Qt.RightButton:
-            self.add_waypoint(scenePos.x(), scenePos.y() - self.waypoint_icon.height())
+            self.add_waypoint(scenePos.x(), scenePos.y())
         else:
             self.setDragMode(QGraphicsView.ScrollHandDrag)
         QGraphicsView.mousePressEvent(self, event)
 
-    # 'mouseReleaseEvent' is used to stop panning of the image upon mouse release
+    # 'mouseReleaseEvent' is used to stop the panning of the image upon the release of the left mouse button
     def mouseReleaseEvent(self, event):
         QGraphicsView.mouseReleaseEvent(self, event)
         if event.button() == Qt.LeftButton:
