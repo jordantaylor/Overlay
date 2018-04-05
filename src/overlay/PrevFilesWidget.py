6e90d215a59fcc585af36bfcdc95d0fefd142b7b
andgonz95 <andgonzalez95@tmau.edu>
2018-04-05 11:47:08 -0500

    harrison's prev files changes

--- src/overlay/PrevFilesWidget.py
+++ src/overlay/PrevFilesWidget.py
@@ -1,5 +1,7 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QSizePolicy
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QSizePolicy, QGridLayout
 from PyQt5.QtCore import QRect, pyqtSlot, pyqtSignal
+from pprint import pprint
+import os
 
 
 class PrevFilesWidget(QWidget):
@@ -14,25 +16,39 @@ class PrevFilesWidget(QWidget):
 	########################################################################
 	changeWidgetSignal = pyqtSignal(int)
 	chosenSaveFileSignal = pyqtSignal(str)
+	selectTifSignal = pyqtSignal(str)
 	def __init__(self):
 		super().__init__()
 		self.initUI()
 	def initUI(self):
-		#layout = QGridLayout()
-		#layout.addWidget(QLabel("PrevFilesWidget"))
 		self.l = QLabel("PrevFilesWidget")
 		self.btn = QPushButton("<-- Back",self)
-		#self.btn.setGeometry(QRect(50,50,150,30))
-
-		self.btn2 = QPushButton("TODO: SAVED WAYPOINTS",self)
-		self.btn2.setSizePolicy( QSizePolicy.Preferred, QSizePolicy.Expanding )
 
 		self.qbly = QVBoxLayout()
 		self.qbly.addWidget(self.btn,1)
-		self.qbly.addWidget(self.btn2,14)
 		self.btn.clicked.connect(self.on_back_clicked)
-		self.setLayout(self.qbly)
+		#self.setLayout(self.qbly)
 
+		self.itemvlist = QVBoxLayout()
+		self.tiflist = {}
+
+		dirlist = os.listdir("../../TIF_Files")
+		for item in dirlist: 
+			if item[0] != '.':
+				if item[len(item)-4:len(item)] == ".tif":
+					itemhlist = QHBoxLayout()
+					itembtn = QPushButton(item[0:len(item)-4],self)
+					itembtn.clicked.connect(lambda *, item=item: self.item_button_clicked("../../TIF_Files/"+item))
+					itemhlist.addWidget(itembtn)
+					self.tiflist[item] = itembtn
+					self.itemvlist.addLayout(itemhlist)
+		self.setLayout(self.itemvlist)
+		
+
+	@pyqtSlot(str)
+	def item_button_clicked(self,itemName):
+		self.selectTifSignal.emit(itemName) 
+		
 	@pyqtSlot()
 	def on_back_clicked(self):
 		self.changeWidgetSignal.emit(0)
