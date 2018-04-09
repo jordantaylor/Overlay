b6af920c910dcea123aa5f21449b8cffb20ef835
andgonz95 <andgonzalez95@tmau.edu>
2018-04-09 14:29:31 -0500

    added3.6 to setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import subprocess
 ver = platform.architecture()[0]
 
 if(ver == '32bit'):
-    subprocess.call("pip install GDAL-2.2.4-cp36-cp36m-win32.whl")
+    subprocess.call("pip3.6 install GDAL-2.2.4-cp36-cp36m-win32.whl")
 else:
-    subprocess.call("pip install GDAL-2.2.4-cp36-cp36m-win_amd64.whl")
-subprocess.call("pip install pyqt5")
+    subprocess.call("pip3.6 install GDAL-2.2.4-cp36-cp36m-win_amd64.whl")
+subprocess.call("pip3.6 install pyqt5")
